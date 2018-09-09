49b4974672d38ce4eb76ae921a1ef310af7d86ab
chf1985 <43111152+chf1985@users.noreply.github.com>
2018-09-09 15:47:52 +0200

    v_0.6

---
+++ chrizz_ultrasearch_v_0.6.bat
@@ -0,0 +1,84 @@
+@echo off
+::#####################################################
+::################ Chrizz Ultra Search ################
+::####################### v 0.6 #######################
+::#####################################################
+::# 
+
+
+
+set /p SUCHE=Hier gesuchten Artikel eingeben:
+set SUCHE=%SUCHE: =+%
+
+
+
+
+IF "%land%" == "de" (goto de) 
+IF "%land%" == "at" (goto at) 
+
+:de
+
+:: URL ohne &
+start https://www.google.%land%/search?q=%SUCHE%
+start https://www.otto.%land%/suche/%SUCHE%/
+start https://www.expert.%land%/suche?q=%SUCHE%
+start https://www.conrad.%land%/de/Search.html?search=%SUCHE%
+start https://www.schwanthaler-computer.%land%/search?sSearch=%SUCHE%
+start https://www.notebooksbilliger.%land%/produkte/%SUCHE%
+start https://www.saturn.%land%/de/search.html?query=%SUCHE%&searchProfile=onlineshop&channel=sedede
+
+
+:: URL´s mit &
+:: Amazon
+set url=start "" "https://www.amazon.%land%/s?url=search-alias+3Daps&field-keywords="
+set url=%url%%SUCHE%
+%url%
+
+::Media Markt
+set url=start "" "https://www.mediamarkt.%land%/de/search.html?query=%SUCHE%&searchProfile=onlineshop&channel=mmdede"
+set url=%url%%SUCHE%
+%url%
+
+::Check 24
+set url=start "" "https://preisvergleich.check24.%land%/suche.html?query=%SUCHE%&searchtype=hd_searchredirect"
+set url=%url%%SUCHE%
+%url%
+
+::Alternate
+set url=start "" "https://www.alternate.%land%/html/search.html?query=%SUCHE%&x=0
+set url=%url%%SUCHE%
+%url%
+
+goto ende
+
+:at
+
+:: URL ohne &
+start https://www.google.%land%/search?q=%SUCHE%
+start https://www.conrad.%land%/de/Search.html?search=%SUCHE%
+start https://www.saturn.%land%/de/search.html?query=%SUCHE%&searchProfile=onlineshop&channel=sedede
+
+
+
+
+:: URL´s mit &
+::Alternate
+set url=start "" "https://www.alternate.%land%/html/search.html?query=%SUCHE%&x=0
+set url=%url%%SUCHE%
+%url%
+
+::Media Markt
+set url=start "" "https://www.mediamarkt.%land%/de/search.html?query=%SUCHE%&searchProfile=onlineshop&channel=mmdede"
+set url=%url%%SUCHE%
+%url%
+
+goto ende
+
+:ende
+exit
+
+
+
+
+
+

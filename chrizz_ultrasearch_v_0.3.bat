baa6e2576dc3eb6772f80fb4f26416a16f100775
chf1985 <43111152+chf1985@users.noreply.github.com>
2018-09-09 14:40:00 +0200

    v_0.2

---
+++ chrizz_ultrasearch_v_0.3.bat
@@ -0,0 +1,44 @@
+@echo on
+::#####################################################
+::################ Chrizz Ultra Search ################
+::####################### v 0.3 #######################
+::#####################################################
+::#                                                   #
+::#    Dieses Script öffnet gesuchte Artikel im       #
+::#	     Standard Browser der mit der             #
+::#    Suchengine verschiedener Anbieter und Hilft    #
+::#   einen überblick zu schaffe, wo ein Artikel am   #
+::#           Günstigsten erhältlich ist.             #
+::#                                                   #	
+::# ###################################################
+::#                                                   #
+::#     Für erweiterungen mittels mehr Anbieter       #
+::#                 bitte E-Mail an		      #
+::#                chf1985@gmail.com                  #
+::#                                                   #
+::# ###################################################
+
+set /p SUCHE=Hier gesuchten Artikel eingeben:
+set SUCHE=%SUCHE: =+%
+
+:: URL ohne &
+start https://www.google.de/search?q=%SUCHE%
+start https://www.otto.de/suche/%SUCHE%/
+start https://www.expert.de/suche?q=%SUCHE%
+start https://www.conrad.de/de/Search.html?search=%SUCHE%
+start https://www.schwanthaler-computer.de/search?sSearch=%SUCHE%
+start https://www.notebooksbilliger.de/produkte/%SUCHE%
+
+:: URL´s mit &
+:: Amazon
+set url=start "" "https://www.amazon.de/s?url=search-alias+3Daps&field-keywords="
+set url=%url%%SUCHE%
+%url%
+
+::Media Markt
+set url=start "" "https://www.mediamarkt.de/de/search.html?query=%SUCHE%&searchProfile=onlineshop&channel=mmdede"
+set url=%url%%SUCHE%
+%url%
+
+::Saturn
+set url=start "" "https://www.saturn.de/de/search.html?query=%SUCHEN%+x&searchProfile=onlineshop&channel=sedede"
---
+++ chrizz_ultrasearch_v_0.5.bat
@@ -0,0 +1,54 @@
+@echo off
+::#####################################################
+::################ Chrizz Ultra Search ################
+::####################### v 0.5 #######################
+::#####################################################
+::#                                                   #
+::#    Dieses Script öffnet gesuchte Artikel im       #
+::#	     Standard Browser der mit der             #
+::#    Suchengine verschiedener Anbieter und Hilft    #
+::#   einen überblick zu schaffe, wo ein Artikel am   #
+::#           Günstigsten erhältlich ist.             #
+::#                                                   #	
+::# ###################################################
+::#                                                   #
+::#     Für erweiterungen mittels mehr Anbieter       #
+::#                 bitte E-Mail an		      #
+::#                chf1985@gmail.com                  #
+::#                                                   #
+::# ###################################################
+
+set /p SUCHE=Hier gesuchten Artikel eingeben:
+set SUCHE=%SUCHE: =+%
+
+:: URL ohne &
+start https://www.google.de/search?q=%SUCHE%
+start https://www.otto.de/suche/%SUCHE%/
+start https://www.expert.de/suche?q=%SUCHE%
+start https://www.conrad.de/de/Search.html?search=%SUCHE%
+start https://www.schwanthaler-computer.de/search?sSearch=%SUCHE%
+start https://www.notebooksbilliger.de/produkte/%SUCHE%
+start https://www.saturn.de/de/search.html?query=%SUCHE%&searchProfile=onlineshop&channel=sedede
+
+
+:: URL´s mit &
+:: Amazon
+set url=start "" "https://www.amazon.de/s?url=search-alias+3Daps&field-keywords="
+set url=%url%%SUCHE%
+%url%
+
+::Media Markt
+set url=start "" "https://www.mediamarkt.de/de/search.html?query=%SUCHE%&searchProfile=onlineshop&channel=mmdede"
+set url=%url%%SUCHE%
+%url%
+
+::Check 24
+set url=start "" "https://preisvergleich.check24.de/suche.html?query=%SUCHE%&searchtype=hd_searchredirect"
+set url=%url%%SUCHE%
+%url%
+
+::Alternate
+set url=start "" "https://www.alternate.de/html/search.html?query=%SUCHE%&x=0
+set url=%url%%SUCHE%
+%url%
+exit
